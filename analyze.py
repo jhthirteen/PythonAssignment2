57727adf6c75ab2be06bc7835b9aaca8b00e7a51
Jack Hunter <jhunter5@binghamton.edu>
2025-10-04 15:03:48 -0400

    added code outline and hint comments

---
+++ analyze.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+"""
+For reading a CSV file as a dataframe, you can use the built-in function
+pd.read_csv('path_to_file_name') where path_to_file_name is the name of the CSV file
+"""
+
+"""
+To loop through a DataFrame, we can use the structure:
+for index, row in df.iterrows():
+    loop_body
+where index is the row index, and row is the key: value pairs for that specific row
+"""
+
+"""
+In a single row, to access the value associated with a column key, you can use the syntax row[key]
+For example, if you are given a row with a column called "Name", you could access the value with row["Name]
+"""
+
+def main():
+    #TODO: Implement steps 1 through 5
+    pass
+
+main()
